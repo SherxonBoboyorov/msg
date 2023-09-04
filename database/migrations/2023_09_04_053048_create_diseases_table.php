198d0494429bf9b93d2f6125eb0453ef26bedfb5
Sherxon <116145014+SherxonBoboyorov@users.noreply.github.com>
2023-09-04 10:52:50 +0500

    changes

---
+++ database/migrations/2023_09_04_053048_create_diseases_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('diseases', function (Blueprint $table) {
+            $table->id();
+
+            $table->text('content_de');
+            $table->text('content_en');
+            $table->text('content_ru');
+
+            $table->string('meta_title_de')->nullable();
+            $table->string('meta_title_en')->nullable();
+            $table->string('meta_title_ru')->nullable();
+
+            $table->text('meta_description_de')->nullable();
+            $table->text('meta_description_en')->nullable();
+            $table->text('meta_description_ru')->nullable();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('diseases');
+    }
+};
